6865b82204a9f0ba383d65e28f656addfd799f5f
Tony Ta <hey@tonyta.com>
2023-09-14 11:44:28 -0700

    Option to set all permutations of cookie attributes

--- inner/app/controllers/cookies_controller.rb
+++ inner/app/controllers/cookies_controller.rb
@@ -9,12 +9,29 @@ class CookiesController < ApplicationController
   end
 
   def create
-    cookie_data = {}
-    cookie_data[:value] = "true"
-    cookie_data[:secure] = params[:secure] == "1"
-    cookie_data[:httponly] = params[:httponly] == "1"
-    cookie_data[:same_site] = params[:same_site]
-    cookies[params.require(:key)] = cookie_data
+    cookie_name = params.require(:key)
+
+    if cookie_name == "all"
+      cookies["secure-httponly-none"]         = { value: true, secure: true,  httponly: true,  same_site: :none   }
+      cookies["secure-httponly-lax"]          = { value: true, secure: true,  httponly: true,  same_site: :lax    }
+      cookies["secure-httponly-strict"]       = { value: true, secure: true,  httponly: true,  same_site: :strict }
+      cookies["nonsecure-httponly-none"]      = { value: true, secure: false, httponly: true,  same_site: :none   }
+      cookies["nonsecure-httponly-lax"]       = { value: true, secure: false, httponly: true,  same_site: :lax    }
+      cookies["nonsecure-httponly-strict"]    = { value: true, secure: false, httponly: true,  same_site: :strict }
+      cookies["secure-nonhttponly-none"]      = { value: true, secure: true,  httponly: false, same_site: :none   }
+      cookies["secure-nonhttponly-lax"]       = { value: true, secure: true,  httponly: false, same_site: :lax    }
+      cookies["secure-nonhttponly-strict"]    = { value: true, secure: true,  httponly: false, same_site: :strict }
+      cookies["nonsecure-nonhttponly-none"]   = { value: true, secure: false, httponly: false, same_site: :none   }
+      cookies["nonsecure-nonhttponly-lax"]    = { value: true, secure: false, httponly: false, same_site: :lax    }
+      cookies["nonsecure-nonhttponly-strict"] = { value: true, secure: false, httponly: false, same_site: :strict }
+    else
+      cookie_data = {}
+      cookie_data[:value] = "true"
+      cookie_data[:secure] = params[:secure] == "1"
+      cookie_data[:httponly] = params[:httponly] == "1"
+      cookie_data[:same_site] = params[:same_site]
+      cookies[params.require(:key)] = cookie_data
+    end
 
     redirect_to params[:redirect_url] || sessions_path, allow_other_host: true
   end
